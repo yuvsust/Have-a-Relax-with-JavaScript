a966aa35e7276bf43184cb78f1e06011659dea6c
Tanvir Hossain Yuvraz <tanvirhossainyuvraz@gmail.com>
2024-02-05 04:43:48 +0600

    4. Drum Kit - Added a volume bar to make it better.

--- 4. Drum Kit Wesbos 1-30/app.js
+++ 4. Drum Kit Wesbos 1-30/app.js
@@ -1,3 +1,8 @@
+const keys = document.querySelectorAll('.key');
+let volume = document.getElementById('volume-slider');
+
+keys.forEach(key => key.addEventListener('transitionend', removeAnimation));
+
 document.addEventListener("keydown", (event) => {
     let key_num = event.code;
     let audio = document.querySelector(`audio[data-key=${key_num}]`);
@@ -9,6 +14,7 @@ document.addEventListener("keydown", (event) => {
 
     // Play Audio
     audio.currentTime = 0;
+    audio.volume = volume.value / 100;
     audio.play();
 })
 
@@ -17,5 +23,12 @@ function removeAnimation(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove("playing");
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeAnimation));
+
+// JS for volume slieder
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+    e.style.setProperty('--value', e.value);
+    e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+    e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+    // e.style.setProperty('width', '50%');
+    e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+  }
